20edf50d0ecd3864a70fce9adbf1ce1935d9d3bf
Vijay <vijaythevirtue@gmail.com>
2020-06-18 20:46:29 +0530

    improvement 🚩

--- lib/main.dart
+++ lib/main.dart
@@ -26,88 +26,116 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            expandedHeight: 200.0,
-            pinned: true,
-            flexibleSpace: FlexibleSpaceBar(
-              title: Text('FilledStacks'),
+      body: ClipPath(
+        clipper: MyClipper(),
+        child: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              expandedHeight: 200.0,
+              pinned: true,
+              flexibleSpace: FlexibleSpaceBar(
+                title: Text('SilverAppBar'),
+              ),
             ),
-          ),
-          SliverList(
-            delegate: SliverChildListDelegate([
+            SliverList(
+                delegate: SliverChildListDelegate([
               SizedBox(height: 5),
               Container(
                 width: double.infinity,
                 height: 50,
                 color: Colors.red,
               ),
-              SizedBox(height: 5),
-              Container(
-                width: double.infinity,
-                height: 50,
-                color: Colors.red,
-              ),
-              SizedBox(height: 5),
-              Container(
-                width: double.infinity,
-                height: 50,
-                color: Colors.red,
-              ),
-              SizedBox(height: 5),
-              Container(
-                width: double.infinity,
-                height: 50,
-                color: Colors.red,
-              ),
-              SizedBox(height: 5),
-              Container(
-                width: double.infinity,
-                height: 50,
-                color: Colors.red,
-              ),
-              SizedBox(height: 5),
-              Container(
-                width: double.infinity,
-                height: 50,
-                color: Colors.red,
-              ),
-              SizedBox(height: 5),
-              Container(
-                width: double.infinity,
-                height: 50,
-                color: Colors.red,
-              ),
-              SizedBox(height: 5),
-              Container(
-                width: double.infinity,
-                height: 50,
-                color: Colors.red,
-              ),
-              SizedBox(height: 5),
-              Container(
-                width: double.infinity,
-                height: 50,
-                color: Colors.red,
-              ),
-              SizedBox(height: 5),
-              Container(
-                width: double.infinity,
-                height: 50,
-                color: Colors.red,
-              ),
-              SizedBox(height: 5),
-              Container(
-                width: double.infinity,
-                height: 50,
-                color: Colors.red,
-              ),
-              SizedBox(height: 5),
-            ]),
-          )
-        ],
+            ]))
+            //     SizedBox(height: 5),
+            //     Container(
+            //       width: double.infinity,
+            //       height: 50,
+            //       color: Colors.red,
+            //     ),
+            //     SizedBox(height: 5),
+            //     Container(
+            //       width: double.infinity,
+            //       height: 50,
+            //       color: Colors.red,
+            //     ),
+            //     SizedBox(height: 5),
+            //     Container(
+            //       width: double.infinity,
+            //       height: 50,
+            //       color: Colors.red,
+            //     ),
+            //     SizedBox(height: 5),
+            //     Container(
+            //       width: double.infinity,
+            //       height: 50,
+            //       color: Colors.red,
+            //     ),
+            //     SizedBox(height: 5),
+            //     Container(
+            //       width: double.infinity,
+            //       height: 50,
+            //       color: Colors.red,
+            //     ),
+            //     SizedBox(height: 5),
+            //     Container(
+            //       width: double.infinity,
+            //       height: 50,
+            //       color: Colors.red,
+            //     ),
+            //     SizedBox(height: 5),
+            //     Container(
+            //       width: double.infinity,
+            //       height: 50,
+            //       color: Colors.red,
+            //     ),
+            //     SizedBox(height: 5),
+            //     Container(
+            //       width: double.infinity,
+            //       height: 50,
+            //       color: Colors.red,
+            //     ),
+            //     SizedBox(height: 5),
+            //     Container(
+            //       width: double.infinity,
+            //       height: 50,
+            //       color: Colors.red,
+            //     ),
+            //     SizedBox(height: 5),
+            //     Container(
+            //       width: double.infinity,
+            //       height: 50,
+            //       color: Colors.red,
+            //     ),
+            //     SizedBox(height: 5),
+            //     Container(
+            //       width: double.infinity,
+            //       height: 50,
+            //       color: Colors.red,
+            //     ),
+            //     SizedBox(height: 5),
+            //   ]),
+            // )
+          ],
+        ),
       ),
     );
+  }
+}
+
+class MyClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(0, size.height);
+    path.quadraticBezierTo(
+        size.width / 3, size.height / 3, size.width, size.height / 5);
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return false;
   }
 }
